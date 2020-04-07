63e99e8f8b56906aada7e5b64e014c72e5e16956
Johann Petzold <johann.petzold@gmail.com>
2020-04-07 13:17:47 +0200

    Mise à jour du print

--- SourceControl/ViewController.swift
+++ SourceControl/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Salut")
+        print("Salut les codeurs")
     }
 
 
